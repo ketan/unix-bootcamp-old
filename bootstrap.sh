b5b3a425be0cbe852930cef03b1a40954515947a
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2013-02-11 10:01:30 +0530

    Install bundler and the shebang

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-bundle install --local --path .bundle --binstubs
+HERE=$(cd `dirname $0` > /dev/null && pwd)
+
+export GEM_HOME=$HERE/.gems
+export GEM_PATH=$GEM_HOME
+export PATH=$GEM_HOME/bin:$PATH
+
+gem install vendor/bundle/bundler-1.2.3.gem --bindir $HERE/bin
+$HERE/bin/bundle install --local --path .bundle --binstubs --shebang `which ruby`
